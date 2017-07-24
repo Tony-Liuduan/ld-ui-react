797511dd46cfa2aab36147cfe90146d475a26a3a
liuduan <liuduan.05.05@163.com>
2017-07-24 21:04:47 +0800

    cityPicker

--- src/layout/Examples/Picker/index.js
+++ src/layout/Examples/Picker/index.js
@@ -12,45 +12,63 @@ import {
 
 import {Picker} from '../../Picker/index';
 
+let tempValue = [];
 class PickerPage extends Component {
 
 	state = {
-		showPicker: false
+		showPicker_province: false,
+		showPicker_city: false,
+		showPicker_district : false,
+		cityValue: ''
 	};
 
-	handleClick(e) {
-		e.preventDefault();
-		this.setState({showPicker: true});
+	handleShow(nextPicker) {
+		this.setState({[nextPicker]: true});
 	}
 
+	handleHide(currentPicker) {
+		this.setState({[currentPicker]: false});
+	}
+	
+	handleCancel() {
+		this.setState({showPicker_province: false, showPicker_city: false, showPicker_district: false});
+	}
+
+	handleSelect(label, cb, firstIndex, lastIndex) {
+		firstIndex ? tempValue = [] : false;
+		tempValue.push(label);
+		lastIndex ? this.setState({cityValue: tempValue.join(' ')}) : false;
+		if (cb) cb();
+	}
+
+	renderCityPicker(pickers) {
+		return pickers.map((picker, index) => {
+			const firstIndex = index === 0;
+			const lastIndex = index === pickers.length-1;
+			return <Picker 
+				key={index}
+				index={index}
+				show={picker.show}
+				onShow={this.handleShow.bind(this, picker.nextPicker)}
+				onHide={this.handleHide.bind(this, picker.currentPicker)}
+				onCancel={this.handleCancel.bind(this)}
+				onSelect={(label, cb) => this.handleSelect(label, cb, firstIndex, lastIndex)}
+				selectCitys={this.state.cityValue.split(" ")}
+				data={picker.data}
+			/>
+		});
+	}
+
+
 	render() {
-		const {showPicker} = this.state;
-		return (
-			<Page title="Picker" subTitle="多列选择器" className="picker">
-				<Cells>
-					<CellsTitle>Picker</CellsTitle>
-					<FormHeader>住宅信息</FormHeader>
-					<FormCell select selectPos="after">
-						<CellLabel>所在地区</CellLabel>
-						<CellControl>
-							<input 
-								type="text" 
-								placeholder="请选择省市区" 
-								readOnly 
-								className="ui-inputselect" 
-								style={{height: "50px"}} 
-								onClick={this.handleClick.bind(this)}
-							/>							
-							<CellArrow direction="down" />
-						</CellControl>
-					</FormCell>
-				</Cells>
-				<Picker 
-					show={showPicker}
-					onCancel={e => this.setState({showPicker: false})}
-					data={[
-						{
+		const {showPicker_province, showPicker_city, showPicker_district, cityValue} = this.state;
+		const cityPickers = [{
+					show: showPicker_province,
+					currentPicker: 'showPicker_province',
+					nextPicker: 'showPicker_city',
+					data: [{
 							"label":"北京市",
+							"checked": true
 						},
 						{
 							"label":"天津市",
@@ -84,9 +102,112 @@ class PickerPage extends Component {
 						},
 						{
 							"label":"吉林市",
-						}
-					]}
-				/>
+						}]
+				},{
+					show: showPicker_city,
+					currentPicker: 'showPicker_city',
+					nextPicker: 'showPicker_district',
+					data: [{
+						"label":"北京市",
+						},
+						{
+							"label":"天津市",
+						},
+						{
+							"label":"南京市",
+						},
+						{
+							"label":"广州市",
+						},
+						{
+							"label":"沈阳市",
+							"checked": true
+						},
+						{
+							"label":"太原市",
+						},
+						{
+							"label":"长沙市",
+						},
+						{
+							"label":"海南市",
+						},
+						{
+							"label":"兰州市",
+						},
+						{
+							"label":"西安市",
+						},
+						{
+							"label":"哈尔滨市",
+						},
+						{
+							"label":"吉林市",
+						}]
+				},{
+					show: showPicker_district,
+					currentPicker: 'showPicker_district',
+					nextPicker: '',
+					data: [{
+						"label":"北京市",
+						},
+						{
+							"label":"天津市",
+						},
+						{
+							"label":"南京市",
+						},
+						{
+							"label":"广州市",
+						},
+						{
+							"label":"沈阳市",
+						},
+						{
+							"label":"太原市",
+						},
+						{
+							"label":"长沙市",
+						},
+						{
+							"label":"海南市",
+							"checked": true
+						},
+						{
+							"label":"兰州市",
+						},
+						{
+							"label":"西安市",
+						},
+						{
+							"label":"哈尔滨市",
+						},
+						{
+							"label":"吉林市",
+						}]
+				}];
+		return (
+			<Page title="Picker" subTitle="多列选择器" className="picker">
+				<Cells>
+					<CellsTitle>Picker</CellsTitle>
+					<FormHeader>住宅信息</FormHeader>
+					<FormCell select selectPos="after">
+						<CellLabel>所在地区</CellLabel>
+						<CellControl>
+							<input 
+								type="text" 
+								placeholder="请选择省市区"
+								readOnly 
+								className="ui-inputselect" 
+								style={{height: "50px"}} 
+								onClick={this.handleShow.bind(this, 'showPicker_province')}
+								value={cityValue}
+							/>							
+							<CellArrow direction="down" />
+						</CellControl>
+					</FormCell>
+				</Cells>
+				{this.renderCityPicker(cityPickers)}
 			</Page>
 		);
 	}
--- src/layout/Picker/Picker.js
+++ src/layout/Picker/Picker.js
@@ -1,7 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
 
-import {Cells, Cell, CellArrow, CellContent} from '../Cell/index';
+import {Cells, Cell, CellArrow, CellContent, FormCell} from '../Cell/index';
 import Mask from '../Mask/index';
 import Navigation from '../Navigation/index';
 
@@ -11,46 +11,80 @@ import '../Base/Css/animate.scss';
 class Picker extends Component {
 	
 	static propTypes = {
+		index: PropTypes.number,
 		show: PropTypes.bool,
 		data: PropTypes.array,
 	};
 
 	static defaultProps = {
+		index: 0,
 		show: false,
 		data: [],
-
 	};
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			close: false
+			close: false,
+			selectedValue: ''
+		};
+	}
+
+	componentDidMount() {
+		this.props.data.map(item => {
+			if (item.checked) {
+				this.setState({selectedValue: item.label})
+			}
+		});
+	}
+
+	handleClose(cb, direction='right') {
+		// 首个picker || 按左箭头 显示动画
+		if (this.props.index === 0 || direction == 'left') {
+			this.setState({
+				close: true
+			}, () => setTimeout(() => {
+				this.setState({
+					close: false
+				});
+				if (cb) cb();
+			}, 300));
+		} else if (cb) {
+			cb();
 		}
 	}
 
-	handleClose(e) {
-		this.setState({
-			close: true
-		}, () => setTimeout(() => {
-			this.setState({
-				close: false
-			});
-			if (this.props.onCancel) this.props.onCancel(e);
-		}, 300));
+	handleSelectItem(e, cb) {
+		this.setState({selectedValue: e.target.value});
+		if (cb) cb();
 	}
 
-	renderPickerBody(data) {
-		return data.map((item, index) => (
-				<Cell key={index}>
-					<CellContent onClick={e => console.log(item.label)}>{item.label}</CellContent>
-				</Cell>
-			));
+	renderNavigation(option) {
+		return  <Navigation
+					title={option.title}
+					left={option.left}
+					leftClick={option.leftClick}
+					rightClick={option.rightClick}
+					rightContent={option.rightContent}
+				/>;
+	}
+
+	renderPickerBody(data, onSelect, cb) {
+		return data.map((item, index) => {
+			const selectCls = classNames({'ui-picker-selected': this.state.selectedValue === item.label});
+			item.checked = this.state.selectedValue === item.label;
+
+			return (
+				<FormCell radio key={index} className={selectCls} onClick={(e) => this.handleSelectItem(e, () => onSelect(item.label, cb))}>
+					<CellContent><input readOnly value={item.label} /></CellContent>
+				</FormCell>
+			);
+		});
 	}
 
 	render() {
-		const {show, onCancel, data, className, children, ...other} = this.props;
+		const {index, show, onShow, onHide, onCancel, onSelect, data, selectCitys, className, ...other} = this.props;
 		const {close} = this.state;
-
 		const cls = classNames({
 			'ui-picker': true,
 			'ui-animate-slide-up': show && !close,
@@ -60,19 +94,37 @@ class Picker extends Component {
 			'ui-animate-fade-in': show && !close,
 			'ui-animate-fade-out': close
 		});
+		const Navgroup = [{
+						title: '选择省',
+						left: false,
+						rightClick: () => this.handleClose(onCancel),
+						rightContent: '取消'
+					},{
+						title: '选择市',
+						left: true,
+						leftClick: () => this.handleClose(onHide, 'left'),
+						rightClick: () => this.handleClose(onCancel),
+						rightContent: '取消'
+					},{
+						title: '选择区/县',
+						left: true,
+						leftClick: () => this.handleClose(onHide, 'left'),
+						rightClick: () => this.handleClose(onCancel),
+						rightContent: '取消'
+					}];
+		const transparent = index === 0 ? false : true;
 
 		return show ? (
 			<div>
-				<Mask className={maskCls} onClick={this.handleClose.bind(this)}/>
+				<Mask className={maskCls} transparent={transparent} onClick={this.handleClose.bind(this, onCancel)}/>
 				<div className={cls} {...other}>
-					<Navigation
-						title="选择市"
-						left={false}
-						rightContent="取消"
-						rightClick={this.handleClose.bind(this)}
-					/>
+					{this.renderNavigation(Navgroup[index])}
 					<div className="ui-picker-body">
-						{this.renderPickerBody(data)}
+						{
+							index == Navgroup.length-1 
+								? this.renderPickerBody(data, onSelect, onCancel)
+								: this.renderPickerBody(data, onSelect, onShow) 
+						}
 					</div>
 				</div>
 			</div>
